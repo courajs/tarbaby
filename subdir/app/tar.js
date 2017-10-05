0c596ed18fd14c679696d6846a1ff956267576ba
Aaron Sikes <aaron@sikes.io>
2017-10-05 01:17:42 -0400

    Ok, using my linked modified tarfs it works

--- subdir/app/tar.js
+++ subdir/app/tar.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
 const dockerfilejs = require('dockerfilejs');
-const tarfs = require('tar-fs');
-const tar = require('tar-stream');
+const tar = require('tar-fs');
 
 
 let packages = [
@@ -12,101 +11,65 @@ let packages = [
 ];
 
 
-function entryStreamForPackage(package) {
-  let pack = tarfs.pack(package.path, {
-    ignore: (name) => name.indexOf('node_modules') !== -1,
-    map: (header) => {
-      let newName = path.normalize(path.join(package.name, header.name));
-      header.name = newName;
-      return header;
-    }
-  });
-  return pack.pipe(tar.extract());
-}
-
-function entryStreamForApp(appPath) {
-  let pack = tarfs.pack(appPath, {
-    ignore: (name) => name.indexOf('node_modules') !== -1,
-    map: (header) => {
-      let newName = path.normalize(path.join('app', header.name));
-      header.name = newName;
-      return header;
-    }
-  });
-  return pack.pipe(tar.extract());
-}
-
-let context = tar.pack();
-
-async function pipeToContext(entryStream, prefix='') {
-  console.log('gonna pipe to context');
-  entryStream.on('entry', function(header, stream, next) {
-    console.log('found entry', header.name);
-    header.name = path.normalize(path.join(prefix, header.name));
-    stream.pipe(context.entry(header, next));
-  });
-  console.log('added the listener');
-  return new Promise(function(resolve, reject) {
-    entryStream.on('finish', function(result) {
-      console.log('finish', arguments);
-      resolve(result);
-    });
-  });
-}
-
+go();
 async function go() {
-  let out = context.pipe(tar.extract());
+  let pack = await packApp();
 
-  let app = entryStreamForApp('.');
-  console.log('start go');
-  await pipeToContext(app);
-  console.log('piped app');
-  let p1 = entryStreamForPackage(packages[0]);
-  await pipeToContext(p1, 'packages');
-  console.log('piped p1');
-  let p2 = entryStreamForPackage(packages[1]);
-  await pipeToContext(p2, 'packages');
-  console.log("piped p2");
-  context.finalize();
+  let outStream = fs.createWriteStream('context.tar');
+  pack.pipe(outStream);
 
-  out.on('entry', function(header, stream, next) {
-    console.log('header', header.name);
-    next();
+  for (let package of packages) {
+    await packPackage(pack, package);
+  }
+  pack.entry({name: 'generated.txt'}, 'hello');
+  pack.finalize();
+  console.log('wow! done.');
+}
+
+async function packApp() {
+  return new Promise(function(resolve, reject) {
+    let pack = tar.pack('.', {
+      ignore: or(node_modules, tarfiles),
+      map(header) {
+        let newName = path.normalize(path.join('app', header.name));
+        console.log('app mapping', header.name, 'to', newName);
+        header.name = newName;
+        return header;
+      },
+      finalize: false,
+      finish: resolve
+    })
+    pack.on('error', reject);
   });
 }
 
-go().then(function(result) {
-  console.log('done', result);
-}, function(err) {
-  console.log('error', err);
-});
+function node_modules(name) {
+  return path.basename(name) === 'node_modules';
+}
+function tarfiles(name) {
+  return path.extname(name) === '.tar';
+}
+function or() {
+  let predicates = [].slice.call(arguments);
+  return (name) => {
+    return [].reduce.call(predicates, (a,b)=>a||b(name), false);
+  }
+}
 
-
-/*
-let packages = child_process.spawn('tar', [
-    '-c',
-    '--exclude', 'node_modules',
-    '-s', ",^,packages/,",
-    '-C', '/Users/aaron/dev/tarbaby', 'proj',
-    '-C', '/Users/aaron/dev/tarbaby/subdir', 'other',
-], {
-  stdio: ['ignore', 'pipe', 'inherit']
-});
-
-let context = child_process.spawn('tar', [
-    '-c',
-    '--exclude', 'node_modules',
-    'Dockerfile',
-    '-C', '/Users/aaron/dev/tarbaby/subdir', 'app',
-    '@-'
-], {
-  stdio: ['pipe', 'pipe', 'inherit']
-});
-
-
-packages.stdout.pipe(context.stdin);
-
-
-let outStream = fs.createWriteStream('/Users/aaron/dev/tarbaby/temp.tar');
-context.stdout.pipe(outStream);
-*/
+async function packPackage(pack, package) {
+  return new Promise(function(resolve, reject) {
+    tar.pack(package.path, {
+      pack,
+      finalize: false,
+      ignore: node_modules,
+      map(header) {
+        let newName = path.normalize(path.join(package.name, header.name));
+        console.log('package mapping', header.name, 'to', newName);
+        header.name = newName;
+        return header;
+      },
+      finish: resolve
+    })
+    pack.on('error', reject);
+  });
+}
